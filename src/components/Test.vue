914f408a2e4c2ec2747beda2a6093578384ded0f
ChenJiaLiang <chen123jialiang@163.com>
2022-12-07 00:44:56 +0800

    分类 增删改查 接口

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,19 +1,40 @@
 <script lang="ts">
 import { defineComponent, reactive, onMounted } from "vue";
-import { test } from "@/api/request";
+import { test, addItem, deleteItem, changeItem, queryAll } from "@/api/request";
 export default defineComponent({
   setup() {
     const state = reactive({
       test: "test666",
     });
     onMounted(() => {
-      test(1)
-        .then((res) => {
-          debugger;
-        })
-        .catch((err) => {
-          debugger;
-        });
+      // 新增
+      // const param = {
+      //   name: "vue",
+      //   createTime: new Date().getTime().toString(),
+      //   studyLink: "http://baidu.com",
+      // };
+      // addItem(param);
+
+      // 删除
+      // const param = {
+      //   id: 3,
+      // };
+      // deleteItem(param);
+
+      // 修改
+      const param = {
+        id: 1,
+        name: "vue3",
+        count: 1,
+        createTime: new Date().getTime().toString(),
+        studyLink: "http://baidu.com4",
+        priority: 9,
+        isEffective: "1",
+      };
+      changeItem(param);
+
+      // 查询
+      queryAll();
     });
     return {
       state,
