2bf8d706e3c83fcc4d311684925b04ba40e59f5b
ChenJiaLiang <chen123jialiang@163.com>
2022-12-08 23:08:56 +0800

    marked highlight.js 库的使用引入

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import test from "@/components/Test.vue";
+import mar from "@/components/marked.vue";
 export default defineComponent({
   name: "App",
-  components: { test },
+  components: { test, mar },
 });
 </script>
 
 <template>
   <div class="layout">
     <test></test>
+    <mar></mar>
   </div>
 </template>
 
